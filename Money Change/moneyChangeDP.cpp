ff22d3b4a015401ffaf467ecd0546a370f31ec19
Mr. Twinkle Sharma <smtwinkle451@gmail.com>
2020-09-29 05:08:27 +0530

    With Dynamic Programming O(m*n)

--- Money Change/moneyChangeDP.cpp
+++ Money Change/moneyChangeDP.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include<algorithm>
 using namespace std;
-int get_change(int money) {
-   int coins[] = {1,5,7};
+int get_change(int money, int dp[]) {
+   int coins[] = {1,3,4};
    
    //checking whether we reached at end means we have changed all money with our denominations
    if(money==0)
@@ -18,7 +18,13 @@ int get_change(int money) {
    	    	int subAns = 0;   //Initializing with 0 or can say simply creating a variable which can hold the value of subAns of Problem
    			
 			//Main Recursion 
-			subAns = get_change(money-coins[i]);
+			if(dp[money-coins[i]] != -1 )    //if DP array has already a value stored for any money then simply use it 
+			subAns = dp[money - coins[i]];
+			
+			//otherwise do recursion
+			else{
+			subAns = get_change(money-coins[i],dp);
+			}
 			
 			//if ans is not filled yet or filled but more subAns has more optimal answer then update it 
 			if(subAns+1 < ans || ans ==-1)
@@ -26,14 +32,22 @@ int get_change(int money) {
 		}
    	    
    }
-  return ans;
+    //Storing the optimal number of coins to change for that money at the index
+  	dp[money]=ans;  
+  	return ans;
+	//Reduce Steps using 
+	return dp[money] = ans;
 }
 
 int main() {
   int m;
   std::cin >> m;
+  //Just more than one the money to change because we are initalizing with 0 for 0
+  int dp[m+1];  
   
-  // Final answer to be return by function
-  int ans = get_change(m);
+  //Filling -1 in every index just to indicate that subproblem is not calculated yet
+  fill(dp,dp+m+1,-1);
+
+  int ans = get_change(m,dp);
   std::cout << ans << '\n';
 }
