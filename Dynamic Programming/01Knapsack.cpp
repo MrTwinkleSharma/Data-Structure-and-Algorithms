f4335bc4ca6b309f4bb2cad13312a5de0006a6f2
MrTwinkleSharma <smtwinkle451@gmail.com>
2021-03-24 02:17:45 +0530

    DP 01 Knapsack

--- Dynamic Programming/01Knapsack.cpp
+++ Dynamic Programming/01Knapsack.cpp
@@ -9,6 +9,8 @@ using namespace std;
 	Author: Twinkle Sharma
 */
 
+int dp[1002][1002];
+    
 int knapsack(int wt[],int val[], int W, int n)
 {
 	/*
@@ -21,12 +23,16 @@ int knapsack(int wt[],int val[], int W, int n)
 	// Base Condition
 	if(n==0 || W==0)
 		return 0;
-		
+	
+	
+	if(dp[W][n]!=-1)
+		return dp[W][n];
+
 	//Choice Based Recursion
 	if(W>=wt[n-1])
-		return max(val[n-1]+knapsack(wt,val, W-wt[n-1], n-1), knapsack(wt, val, W, n-1));
+		return dp[W][n] = max(val[n-1]+knapsack(wt,val, W-wt[n-1], n-1), knapsack(wt, val, W, n-1));
 	else
-		return knapsack(wt,val,W,n-1);	
+		return dp[W][n] = knapsack(wt,val,W,n-1);	
 }
 int main()
 {
@@ -37,6 +43,9 @@ int main()
 	int wt[n], val[n]; 	
 	cout<<"Two Arrays Weights and Values\n";
 	
+	
+	
+	memset(dp,-1,sizeof(dp));
 	for(int i=0;i<n;i++)
 		cin>>wt[i];
 	for(int i=0;i<n;i++)
